656d628ed03c0042be659c3614dfe97afb74c0db
Balazs Balogh <baloghbalazs646@gmail.com>
2021-01-26 21:58:22 +0100

    updated to three input fields

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ db = SQLAlchemy(app)
 
 class Calcs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(30), nullable=False)
+    content_x = db.Column(db.String(30), nullable=False, default='0')
+    content_y = db.Column(db.String(30), nullable=False, default='0')
+    content_z = db.Column(db.String(30), nullable=False, default='0')
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __repr__(self):
@@ -22,8 +24,11 @@ class Calcs(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content1']
-        new_task = Calcs(content=task_content)
+        content_x_in = request.form['content1']
+        content_y_in = request.form['content2']
+        content_z_in = request.form['content3']
+
+        new_task = Calcs(content_x=content_x_in, content_y=content_y_in, content_z=content_z_in)
 
         try:
             db.session.add(new_task)
@@ -51,7 +56,9 @@ def update(id):
     task = Calcs.query.get_or_404(id)
 
     if request.method == 'POST':
-        task.content = request.form['content1']
+        task.content_x = request.form['content1']
+        task.content_y = request.form['content2']
+        task.content_z = request.form['content3']
 
         try:
             db.session.commit()
